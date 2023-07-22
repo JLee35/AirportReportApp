bef45d2d4f24d9541f7aa3e64e41f55e2be0e6d8
Jared Lee <jared.lee@intellitect.com>
2023-07-21 19:03:58 -0700

    Adds blank airport service and interface

--- AirportReportUi/src/app/app.module.ts
+++ AirportReportUi/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { SearchBarComponent } from './search-bar/search-bar.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AirportReportUi/src/app/search-bar/search-bar.component.ts
+++ AirportReportUi/src/app/search-bar/search-bar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent {
+  onGoButtonClick() {
+    const searchTerm = (document.querySelector('input') as HTMLInputElement).value;
+    console.log(`Performing serach for: ${searchTerm}`);
+  }
 
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.onGoButtonClick();
+    }
+  }
 }
