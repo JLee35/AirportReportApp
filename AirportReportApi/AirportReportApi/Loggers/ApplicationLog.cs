b8cbe6045f40422f61f127859d4e57d4e4c95646
Jared Lee <jared.lee@intellitect.com>
2023-07-23 15:07:41 -0700

    Had to rename the ApplicationLog directory to be tracked by git

---
+++ AirportReportApi/AirportReportApi/Loggers/ApplicationLog.cs
@@ -0,0 +1,9 @@
+namespace AirportReportApi.Core.Logs;
+
+public class ApplicationLog
+{
+    // Intentionally left blank. Using this as a placeholder because
+    // ILogger needs an associated type. Program.cs is setup to log
+    // to the console. For production purposes, Program.cs would need
+    // to be changed and this class would need to be implemented.
+}
